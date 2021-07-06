e0a7bfb26f3f9cbd624d4c17036c8053a4be0814
Marco Silva <marcosilva.tec@gamil.com>
2021-07-05 23:30:49 -0300

    opcao app

--- app/src/main/java/com/msmaker/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/msmaker/pedrapapeloutesoura/MainActivity.java
@@ -3,6 +3,10 @@ package com.msmaker.pedrapapeloutesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionadoPedra(View view) {
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionadoPapel(View view) {
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionadoTesoura(View view) {
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String opcaoSelecionada) {
+
+        ImageView imagemResultado = findViewById(R.id.imageResultado);
+
+        int numero = new Random().nextInt(3);
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        String opcaoApp = opcoes[numero];
+
+        switch (opcaoApp) {
+            case "pedra":
+                imagemResultado.setImageResource(R.drawable.pedra);
+                break;
+
+            case "papel":
+                imagemResultado.setImageResource(R.drawable.papel);
+                break;
+
+            case "tesoura":
+                imagemResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
     }
 }
